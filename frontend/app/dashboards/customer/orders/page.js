f8c3d8883571ed90b7bcc831c9e46f92738a7e3a
Arshad4456 <mdarshadkhan344@gmail.com>
2026-02-21 00:12:50 +0500

    Update page.js

--- frontend/app/dashboards/customer/orders/page.js
+++ frontend/app/dashboards/customer/orders/page.js
@@ -1,19 +1,6 @@
-import PrimaryOrderRequestModule from "../../components/PrimaryOrderRequestModule";
-import DistributorSecondaryOrdersModule from "../../components/DistributorSecondaryOrdersModule";
-import UserDashboardShell from "../../components/userDashboardShell";
+import SecondaryOrderRequestModule from "../../components/SecondaryOrderRequestModule";
 import { userDashboardSearchItems } from "../../searchItems";
 
 export default function Page() {
-  return (
-    <UserDashboardShell
-      title="Distributor Order Desk"
-      subtitle="Create primary sale order requests and manage receipt agreement from ledger."
-      roleKey="Distributor"
-      links={userDashboardSearchItems.distributor || []}
-      showAccountCards
-    >
-      <PrimaryOrderRequestModule role="Distributor" />
-      <DistributorSecondaryOrdersModule />
-    </UserDashboardShell>
-  );
+  return <SecondaryOrderRequestModule title="Order Management" roleKey="customer" links={userDashboardSearchItems.customer || []} />;
 }
