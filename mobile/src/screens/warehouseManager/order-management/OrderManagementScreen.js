75f348cb8a90a93db1e13a42b995f2ff0aa9eec6
Arshad4456 <133117340+Arshad4456@users.noreply.github.com>
2026-03-09 06:22:51 +0500

    Align warehouse manager order management with website

--- mobile/src/screens/warehouseManager/order-management/OrderManagementScreen.js
+++ mobile/src/screens/warehouseManager/order-management/OrderManagementScreen.js
@@ -1,13 +1,3 @@
-import React from 'react';
-import ModulePlaceholderScreen from '../../common/ModulePlaceholderScreen';
+import OrderManagementScreen from '../../admin/order-management/OrderManagementScreen';
 
-const config = {
-  "route": "/dashboards/warehouseManager/order-management",
-  "moduleKey": "warehouseManager:order-management",
-  "title": "Order Management",
-  "endpoints": []
-};
-
-export default function OrderManagementScreen() {
-  return <ModulePlaceholderScreen config={config} />;
-}
+export default OrderManagementScreen;
