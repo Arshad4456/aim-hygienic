a665c0e33adbd70e234cce40fd84a5d9bbca9655
Arshad4456 <mdarshadkhan344@gmail.com>
2026-03-13 07:11:31 +0500

    update

--- frontend/app/platform-admin/companies/[companyId]/onboarding/page.js
+++ frontend/app/platform-admin/companies/[companyId]/onboarding/page.js
@@ -3,7 +3,8 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useParams } from "next/navigation";
 import { apiFetch } from "../../../../lib/api";
-
+import OnboardingLayout from "../../../components/OnboardingLayout";
+import OnboardingStepper from "../../../components/OnboardingStepper";
 import CompanyCreateStep from "../../../components/onboarding/CompanyCreateStep";
 import CompanySettingsStep from "../../../components/onboarding/CompanySettingsStep";
 import HierarchySelectionStep from "../../../components/onboarding/HierarchySelectionStep";
